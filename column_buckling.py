966292765fd41a88fd9ca388181a971e09f1d199
EvilMorty613 <evgeniymikulov@gmail.com>
2026-05-07 19:45:28 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,16 +1,4 @@
-def find_critical_load(L, E, A, r, c, e, sigma_allow):
-    """
-    L: אורך במ"מ
-    E: מודול אלסטיות ב-MPa
-    A: שטח חתך בממ"ר
-    r: רדיוס אינרציה במ"מ
-    c: מרחק לסיב קיצוני במ"מ
-    e: אקסצנטריות במ"מ
-    sigma_allow: מאמץ מותר ב-MPa
-
-    Return: העומס P בניוטון (float)
-    """
- import numpy as np
+import numpy as np
 from scipy.optimize import bisect
 
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
